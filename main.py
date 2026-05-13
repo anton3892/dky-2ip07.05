35384d09aac29eae9cff902187c9579cbb595b65
Антон <158498612+anton3892@users.noreply.github.com>
2026-05-13 23:15:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,26 +29,42 @@
 # // без остатка
 
 
-m, n = map(int, input("Введете числа: ").split())
-# map превращает их в целые числа
+# m, n = map(int, input("Введете числа: ").split())
+# # map превращает их в целые числа
 
-result = 0
+# result = 0
 
-for j in range(n, m - 1, -1):
-    e = j
-    sums = 0
-    res = False
+# for j in range(n, m - 1, -1):
+#     e = j
+#     sums = 0
+#     res = False
 
-    while e > 0:
-        check = e % 10
+#     while e > 0:
+#         check = e % 10
 
-        if check > 0 and check % 2 == 0:
-            sums += check
-            eve = True
+#         if check > 0 and check % 2 == 0:
+#             sums += check
+#             eve = True
 
-        e //= 10
+#         e //= 10
 
-    if check and sums % 3 == 0:
-        result = j
-        break
-print(result)
+#     if check and sums % 3 == 0:
+#         result = j
+#         break
+# print(result)
+
+#4
+# data = input().split()
+# n, k, m = int(data[0]), int(data[1]), int(data[2])
+#
+# total = 2 * n
+# ones = total - k - m
+#
+# if ones < 0 or (2 * k + ones) % 2 != 0:
+#     print(-1)
+# else:
+#     base3 = [2] * k + [1] * ones + [0] * m
+#     result = []
+#     for i in range(0, total, 2):
+#         result.append(str(base3[i] * 3 + base3[i + 1]))
+#     print(''.join(result))
