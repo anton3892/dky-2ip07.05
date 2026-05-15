420102d020764147bbf1cf501a73860dfb1b5444
Антон <158498612+anton3892@users.noreply.github.com>
2026-05-15 08:38:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,27 @@
-# 3 задача
+import numpy as np
+import scipy.stats as sps
+import matplotlib.pyplot as plt
+from scipy.signal import chirp
 
-# vod = input("Введите данные: ").split()
+x_dist = np.linspace(-1, 2, 1000)
+pdf = sps.uniform.pdf(x_dist, loc=0, scale=1)
+#(равномерного распределения) (плотности вероятности)
+
+plt.figure(1, figsize=(8, 4))
+plt.plot(x_dist, pdf, color='blue', lw=2)
+plt.fill_between(x_dist, pdf, color='blue', alpha=0.2)
+plt.title('Равномерное распределение')
+plt.xlabel('Значение')
+plt.ylabel('Плотность')
+plt.grid(True)
+
+# t = np.linspace(6, 10, 1000)
+# w = chirp(t, f0=4, f1=1, t1=10, method='linear')
 #
-# num = vod[1:]
-#
-# list1 = []
-#
-# for x in num:
-#     f = 1
-#     for j in x:
-#         f = int(j)
-#     list1.append(f)
-#
-# if list1:
-#     generators = 0
-#
-#     for f in list1:
-#         l = list1.count(f) #считаем сколько раз  произведение встречается в списке
-#         if l > generators:
-#             generators = l
-#     print(generators)
-# else:
-#     print(0)
-
-
-# 2 задача
-# %  остатка от деления
-# // без остатка
-
-
-# m, n = map(int, input("Введете числа: ").split())
-# # map превращает их в целые числа
-
-# result = 0
-
-# for j in range(n, m - 1, -1):
-#     e = j
-#     sums = 0
-#     res = False
-
-#     while e > 0:
-#         check = e % 10
-
-#         if check > 0 and check % 2 == 0:
-#             sums += check
-#             eve = True
-
-#         e //= 10
-
-#     if check and sums % 3 == 0:
-#         result = j
-#         break
-# print(result)
-
-#4
-# data = input("Введите данные: ").split()
-# n, k, m = int(data[0]), int(data[1]), int(data[2])
-#
-# total = 2 * n
-# ones = total - k - m
-#
-# if ones < 0 or (2 * k + ones) % 2 != 0:
-#     print(-1)
-# else:
-#     base3 = [2] * k + [1] * ones + [0] * m
-#     result = []
-#     for i in range(0, total, 2):
-#         result.append(str(base3[i] * 3 + base3[i + 1]))
-#     print(''.join(result))
+# plt.figure(2, figsize=(8, 4))
+# plt.plot(t, w, color='red', lw=1.5)
+# plt.title('Сигнал')
+# plt.xlabel('Время')
+# plt.ylabel('Амплитуда')
+# plt.grid(True)
+plt.show()
